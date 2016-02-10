359b760fff35f777d5cd2bf56f936093da55c67d
Glenn Harris <gharris@eklo.com>
2016-02-09 21:28:12 -0500

    working!

--- Controller/Tiny85-Standalone-22H/Tiny85-Standalone-22H.ino
+++ Controller/Tiny85-Standalone-22H/Tiny85-Standalone-22H.ino
@@ -1,37 +1,35 @@
-#include <TinyWireS.h>
-
 /*
  * ATtiny85v 8Mhz @ 3.3v
  *                   +---U---+
  *             Reset | 1   8 | Vcc
- *       NC // (PB3) | 2   7 | (PB2) // NC
- *      POT (PB4/A2) | 3   6 | MOTOR_PWM (PB1)
- *               GND | 4   5 | LED_SPEED (PB0)
+ *       NC // (PB3) | 2   7 | MOTOR_START (PB2)
+ *      POT (PB4/A2) | 3   6 | MOTOR_PWM   (PB1)
+ *               GND | 4   5 | LED_SPEED   (PB0)
  *                   +-------+
  */
 
  
 const byte MOTOR_PWM = 1;
+const byte MOTOR_START = 2;
 const byte POT = A2;
 const byte LED_SPEED = 0;
-const byte MAX_PWM = 80;
+
+// convert max % to max pwm
+const byte MIN_PWM = 20;
+const byte MAX_PWM = 170;
 
 const byte SAMPLE_COUNT = 10;
 
 void setup()
 {
   pinMode(MOTOR_PWM, OUTPUT);
+  pinMode(MOTOR_START, OUTPUT);
   pinMode(POT, INPUT);
   pinMode(LED_SPEED, OUTPUT);
 
-  // use pullups on unused pins
-  pinMode(3, INPUT);
-  digitalWrite(3, HIGH);
-  pinMode(2, INPUT);
-  digitalWrite(2, HIGH);
- 
   digitalWrite(LED_SPEED, LOW);
   digitalWrite(MOTOR_PWM, LOW);
+  digitalWrite(MOTOR_START, LOW);
 }
 
 void loop()
@@ -44,8 +42,21 @@ void loop()
     in += analogRead(POT);
   }
   in = in / SAMPLE_COUNT;
-  out = map(in, 0, 1023, MAX_PWM, 0);
+  // convert to a pwm value
+  out = map(in, 0, 1023, MIN_PWM - 5, MAX_PWM);
 
-  analogWrite(MOTOR_PWM, out);
-  analogWrite(LED_SPEED, map(out, 0, MAX_PWM, 0, 255));
+  // reverse out -- pot hooked up backwards
+  //out = ( 255 - out );
+
+  // turn on/off motor if above/below min volume %
+  if ( out <= MIN_PWM )
+  {
+    digitalWrite(MOTOR_START, LOW);
+  }
+  else
+  {
+    digitalWrite(MOTOR_START, HIGH);
+    analogWrite(MOTOR_PWM, out);
+  }
+  analogWrite(LED_SPEED, out - MIN_PWM);
 }
